ded9a0f8c33a6ec0a938081f0972503fe729a924
Anupal Sharma <anupal.sharma@theiconic.com.au>
2020-12-29 15:45:46 +1100

    fix ci-cd error

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -2,7 +2,7 @@ const { merge } = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFedrationPluign = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
-const { container } = require("webpack");
+const { packageJson } = require("../package.json");
 
 const devConfig = {
   mode: "development",
@@ -18,10 +18,7 @@ const devConfig = {
       remotes: {
         marketing: "marketing@http://localhost:8081/remoteEntry.js",
       },
-      shared: ["react", "react-dom"],
-    }),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
+      shared: packageJson.dependencies,
     }),
   ],
 };
